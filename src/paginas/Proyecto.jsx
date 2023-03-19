e1489cfc9a2de4b90c14d6aba6e8d2d3c68d8862
NestorDanielGomez <nestordanielgomez@gmail.com>
2023-03-19 11:51:19 -0300

    ajustes de tareas y alertas

--- src/paginas/Proyecto.jsx
+++ src/paginas/Proyecto.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import useProyectos from "../hooks/useProyectos";
+import useAdmin from "../hooks/useAdmin";
 import ModalFormularioTarea from "../components/ModalFormularioTarea";
 import ModalEliminarTarea from "../components/ModalEliminarTarea";
 import ModalEliminarColaborador from "../components/ModalEliminarColaborador";
@@ -12,62 +13,67 @@ const Proyecto = () => {
   const params = useParams();
   const { obtenerProyecto, proyecto, cargando, handleModalTarea, alerta } =
     useProyectos();
+  const admin = useAdmin();
 
   useEffect(() => {
     obtenerProyecto(params.id);
   }, []);
 
   const { nombre } = proyecto;
+  if (cargando) return "Cargando.....";
   const { msg } = alerta;
-  return cargando ? (
-    "cargando..."
-  ) : (
+
+  return (
     <>
       <div className="flex justify-between">
         <h1 className="font-black text-4xl">{nombre}</h1>
-        <div className="flex items-center gap-2 text-gray-400 hover:text-black">
+        {admin && (
+          <div className="flex items-center gap-2 text-gray-400 hover:text-black">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+              />
+            </svg>
+            <Link
+              to={`/proyectos/editar/${params.id}`}
+              className="uppercase font-bold">
+              Editar
+            </Link>
+          </div>
+        )}
+      </div>
+      {admin && (
+        <button
+          onClick={handleModalTarea}
+          type="button"
+          className=" flex gap-2 items-center justify-center text-sm px-5 py-3 mt-5 w-full md:w-auto rounded-lg uppercase font-bold bg-sky-400 text-white text-center">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            strokeWidth={1.5}
+            strokeWidth="1.5"
             stroke="currentColor"
             className="w-6 h-6">
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
-              d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+              d="M12 4.5v15m7.5-7.5h-15"
             />
           </svg>
-          <Link
-            to={`/proyectos/editar/${params.id}`}
-            className="uppercase font-bold">
-            Editar
-          </Link>
-        </div>
-      </div>
-      <button
-        onClick={handleModalTarea}
-        type="button"
-        className=" flex gap-2 items-center justify-center text-sm px-5 py-3 mt-5 w-full md:w-auto rounded-lg uppercase font-bold bg-sky-400 text-white text-center">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="w-6 h-6">
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M12 4.5v15m7.5-7.5h-15"
-          />
-        </svg>
-        Nueva Tarea
-      </button>
+          Nueva Tarea
+        </button>
+      )}
+
       <p className="font-bold text-xl mt-10">Tareas del proyecto</p>
-      <div className="md:w-1/3 lg:w-1/4 w-full"></div>
-      {msg && <Alerta alerta={alerta} />}
+
       <div className="bg-white shadow mt-10 rounded-lg">
         {proyecto.tareas?.length ? (
           proyecto.tareas?.map((tarea) => (
@@ -79,26 +85,29 @@ const Proyecto = () => {
           </p>
         )}
       </div>
-      <div className="flex items-center justify-between mt-10">
-        <p className="font-bold text-xl mt-2">Colaboradores</p>
-        <Link
-          to={`/proyectos/nuevo-colaborador/${proyecto._id}`}
-          className="text-gray-400 uppercase font-bold hover:text-black">
-          Añadir
-        </Link>
-      </div>
-
-      <div className="bg-white shadow mt-10 rounded-lg">
-        {proyecto.colaboradores?.length ? (
-          proyecto.colaboradores?.map((colaborador) => (
-            <Colaborador key={colaborador._id} colaborador={colaborador} />
-          ))
-        ) : (
-          <p className="text-center my-5 p-10">
-            No hay colaboradores en este proyecto
-          </p>
-        )}
-      </div>
+      {admin && (
+        <>
+          <div className="flex items-center justify-between mt-10">
+            <p className="font-bold text-xl mt-2">Colaboradores</p>
+            <Link
+              to={`/proyectos/nuevo-colaborador/${proyecto._id}`}
+              className="text-gray-400 uppercase font-bold hover:text-black">
+              Añadir
+            </Link>
+          </div>
+          <div className="bg-white shadow mt-10 rounded-lg">
+            {proyecto.colaboradores?.length ? (
+              proyecto.colaboradores?.map((colaborador) => (
+                <Colaborador key={colaborador._id} colaborador={colaborador} />
+              ))
+            ) : (
+              <p className="text-center my-5 p-10">
+                No hay colaboradores en este proyecto
+              </p>
+            )}
+          </div>
+        </>
+      )}
 
       <ModalFormularioTarea />
       <ModalEliminarTarea />
