9b42d6cf880819bea7476b1c8db8539c0fbb97c9
NestorDanielGomez <nestordanielgomez@gmail.com>
2023-04-17 20:57:26 -0300

    ini sesion

--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [alerta, setAlerta] = useState({});
-
+  const [loading, setLoading] = useState(false);
   const { setAuth, cargando } = useAuth();
 
   const navigate = useNavigate();
@@ -23,6 +23,7 @@ const Login = () => {
       return;
     }
     try {
+      setLoading(true);
       const { data } = await clienteAxios.post(`/usuarios/login`, {
         email,
         password,
@@ -30,6 +31,7 @@ const Login = () => {
       setAlerta({});
       localStorage.setItem("token", data.token);
       setAuth(data);
+      setLoading(false);
       navigate("/proyectos");
     } catch (error) {
       setAlerta({
@@ -45,7 +47,7 @@ const Login = () => {
       <h1 className="text-sky-600 font-bold text-6xl capitalize">
         Iniciar Sesión y Administra tus <span className="text-slate-700">Proyectos</span>
       </h1>
-      {cargando && <p>Iniciando Sesión.....</p>}
+      {loading && <p className="text-center">Iniciando sesión</p>}
       {msg && <Alerta alerta={alerta} />}
       <form onSubmit={handlesubmit} className="my-10 bg-white shadow rounded-lg p-10 ">
         <div className="my-5 ">
